a89587fa89622f447bd09ce9ffca958a6e410bfd
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-08-16 15:26:28 +0600

    Added primary data to card

--- src/pages/Products/ProductCard.jsx
+++ src/pages/Products/ProductCard.jsx
@@ -2,16 +2,17 @@ const ProductCard = ({ car }) => {
   return (
     <div className="card bg-base-100 shadow-xl hover:scale-105 transition-transform">
       <figure>
-        <img src={car.image} className="w-full" alt="Album" />
+        <img src={car.image} className="w-full h-80" alt="Album" />
       </figure>
       <div className="p-8">
         <div className="flex justify-between mb-2">
-          <p className="font-bold">{car.location}</p>
-          <p className="font-bold">Participants: {car.participantCount}</p>
+          <p className="font-bold">Brand: {car.brand}</p>
+          <p className="font-bold">Date: {car.creationDate}</p>
         </div>
-        <h2 className="text-xl font-bold text-green-500 mb-4 text-center">
-          {car.campName}
+        <h2 className="text-xl font-bold bg-black p-1 rounded-xl text-[#F4F4F4] mb-4 text-center">
+          {car.name}
         </h2>
+        <p className="font-bold text-center">Price: ${car.price}</p>
       </div>
     </div>
   );
